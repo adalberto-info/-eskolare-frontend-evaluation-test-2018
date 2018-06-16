a075f58e2c6d50687e46e170ba2c0f328ec63bf7
adalberto-info <illuminati.spiritus@gmail.com>
2018-06-16 02:09:44 -0300

    criação do projeto...

---
+++ app/containers/ListaFakers/listaFakers.jsx
@@ -0,0 +1,374 @@
+const faker = require('faker');
+const ReactDataGrid = require('react-data-grid');
+const exampleWrapper = require('../components/exampleWrapper');
+const React = require('react');
+const { Toolbar, Data: { Selectors }, Editors, Formatters } = require('react-data-grid-addons');
+const update = require('immutability-helper');
+const { AutoComplete: AutoCompleteEditor, DropDownEditor } = Editors;
+const { ImageFormatter } = Formatters;
+
+faker.locale = 'en_GB';
+
+const counties = [
+  { id: 0, title: 'Bedfordshire'},
+  { id: 1, title: 'Berkshire'},
+  { id: 2, title: 'Buckinghamshire'},
+  { id: 3, title: 'Cambridgeshire'},
+  { id: 4, title: 'Cheshire'},
+  { id: 5, title: 'Cornwall'},
+  { id: 6, title: 'Cumbria, (Cumberland)'},
+  { id: 7, title: 'Derbyshire'},
+  { id: 8, title: 'Devon'},
+  { id: 9, title: 'Dorset'},
+  { id: 10, title: 'Durham'},
+  { id: 11, title: 'Essex'},
+  { id: 12, title: 'Gloucestershire'},
+  { id: 13, title: 'Hampshire'},
+  { id: 14, title: 'Hertfordshire'},
+  { id: 15, title: 'Huntingdonshire'},
+  { id: 16, title: 'Kent'},
+  { id: 17, title: 'Lancashire'},
+  { id: 18, title: 'Leicestershire'},
+  { id: 19, title: 'Lincolnshire'},
+  { id: 20, title: 'Middlesex'},
+  { id: 21, title: 'Norfolk'},
+  { id: 22, title: 'Northamptonshire'},
+  { id: 23, title: 'Northumberland'},
+  { id: 24, title: 'Nottinghamshire'},
+  { id: 25, title: 'Northamptonshire'},
+  { id: 26, title: 'Oxfordshire'},
+  { id: 27, title: 'Northamptonshire'},
+  { id: 28, title: 'Rutland'},
+  { id: 29, title: 'Shropshire'},
+  { id: 30, title: 'Somerset'},
+  { id: 31, title: 'Staffordshire'},
+  { id: 32, title: 'Suffolk'},
+  { id: 33, title: 'Surrey'},
+  { id: 34, title: 'Sussex'},
+  { id: 35, title: 'Warwickshire'},
+  { id: 36, title: 'Westmoreland'},
+  { id: 37, title: 'Wiltshire'},
+  { id: 38, title: 'Worcestershire'},
+  { id: 39, title: 'Yorkshire'}
+];
+
+const titles = ['Dr.', 'Mr.', 'Mrs.', 'Miss', 'Ms.'];
+
+class Example extends React.Component {
+  constructor(props, context) {
+    super(props, context);
+    this._columns = [
+      {
+        key: 'id',
+        name: 'ID',
+        width: 80,
+        resizable: true,
+        filterable: true,
+        sortable: true 
+      },
+      {
+        key: 'avartar',
+        name: 'Avartar',
+        width: 60,
+        formatter: ImageFormatter,
+        resizable: true,
+        filterable: true,
+        sortable: true,
+        editable: true,
+        headerRenderer: <ImageFormatter value={faker.image.cats()} />
+      },
+      {
+        key: 'county',
+        name: 'County',
+        editor: <AutoCompleteEditor options={counties}/>,
+        width: 200,
+        resizable: true,
+        sortable: true,
+        editable: true,
+      },
+      {
+        key: 'title',
+        name: 'Title',
+        editor: <DropDownEditor options={titles}/>,
+        width: 200,
+        resizable: true,
+        sortable: true,
+        editable: true,
+        events: {
+          onDoubleClick: function() {
+            console.log('The user double clicked on title column');
+          }
+        }
+      },
+      {
+        key: 'firstName',
+        name: 'First Name',
+        editable: true,
+        width: 200,
+        resizable: true,
+        sortable: true,
+        editable: true,
+      },
+      {
+        key: 'lastName',
+        name: 'Last Name',
+        editable: true,
+        width: 200,
+        resizable: true,
+        sortable: true,
+        editable: true,
+      },
+      {
+        key: 'email',
+        name: 'Email',
+        editable: true,
+        width: 200,
+        resizable: true,
+        sortable: true,
+        editable: true,
+      },
+      {
+        key: 'street',
+        name: 'Street',
+        editable: true,
+        width: 200,
+        resizable: true,
+        sortable: true,
+        editable: true,
+      },
+      {
+        key: 'zipCode',
+        name: 'ZipCode',
+        editable: true,
+        width: 200,
+        resizable: true,
+        sortable: true,
+        editable: true,
+      },
+      {
+        key: 'date',
+        name: 'Date',
+        editable: true,
+        width: 200,
+        resizable: true,
+        sortable: true,
+        editable: true,
+      },
+      {
+        key: 'bs',
+        name: 'bs',
+        editable: true,
+        width: 200,
+        resizable: true,
+        sortable: true,
+        editable: true,
+      },
+      {
+        key: 'catchPhrase',
+        name: 'Catch Phrase',
+        editable: true,
+        width: 200,
+        resizable: true,
+        sortable: true,
+        editable: true,
+      },
+      {
+        key: 'companyName',
+        name: 'Company Name',
+        editable: true,
+        width: 200,
+        resizable: true,
+        sortable: true,
+        editable: true,
+      },
+      {
+        key: 'sentence',
+        name: 'Sentence',
+        editable: true,
+        width: 200,
+        resizable: true,
+        sortable: true,
+        editable: true,
+      }
+    ];
+
+    let originalRows = this.createRows(1000);
+    let rows = originalRows.slice(0);
+    this.state = { rows: this.createRows(1000), originalRows, filters: {}, value: 1, scrollToRowIndex: 0};
+  }
+
+  createRows = (numberOfRows) => {
+    let rows = [];
+    for (let i = 0; i < numberOfRows; i++) {
+      rows[i] = this.createFakeRowObjectData(i);
+    }
+    return rows;
+  };
+
+  createFakeRowObjectData = (index) => {
+    return {
+      id: 'id_' + index,
+      avartar: faker.image.avatar(),
+      county: faker.address.county(),
+      email: faker.internet.email(),
+      title: faker.name.prefix(),
+      firstName: faker.name.firstName(),
+      lastName: faker.name.lastName(),
+      street: faker.address.streetName(),
+      zipCode: faker.address.zipCode(),
+      date: faker.date.past().toLocaleDateString(),
+      bs: faker.company.bs(),
+      catchPhrase: faker.company.catchPhrase(),
+      companyName: faker.company.companyName(),
+      words: faker.lorem.words(),
+      sentence: faker.lorem.sentence()
+    };
+  };
+
+  handleGridSort = (sortColumn, sortDirection) => {
+    const comparer = (a, b) => {
+      if (sortDirection === 'ASC') {
+        return (a[sortColumn] > b[sortColumn]) ? 1 : -1;
+      } else if (sortDirection === 'DESC') {
+        return (a[sortColumn] < b[sortColumn]) ? 1 : -1;
+      }
+    };
+
+    const rows = sortDirection === 'NONE' ? this.state.originalRows.slice(0) : this.state.rows.sort(comparer);
+
+    this.setState({ rows });
+  };
+
+  rowGetter = (rowIdx) => {
+    let rows = this.getRows();
+    return rows[rowIdx];
+  };
+
+  getRows = () => {
+    return Selectors.getRows(this.state);
+  };
+
+  getSize = () => {
+    return this.getRows().length;
+  };
+
+  getColumns = () => {
+    let clonedColumns = this._columns.slice();
+    clonedColumns[2].events = {
+      onClick: (ev, args) => {
+        const idx = args.idx;
+        const rowIdx = args.rowIdx;
+        this.grid.openCellEditor(rowIdx, idx);
+      }
+    };
+
+    return clonedColumns;
+  };
+
+  handleFilterChange = (filter) => {
+    let newFilters = Object.assign({}, this.state.filters);
+    if (filter.filterTerm) {
+      newFilters[filter.column.key] = filter;
+    } else {
+      delete newFilters[filter.column.key];
+    }
+    this.setState({ filters: newFilters });
+  };
+
+  onClearFilters = () => {
+    // all filters removed
+    this.setState({filters: {} });
+  };
+  
+  handleGridRowsUpdated = ({ fromRow, toRow, updated }) => {
+    let rows = this.state.rows.slice();
+
+    for (let i = fromRow; i <= toRow; i++) {
+      let rowToUpdate = rows[i];
+      let updatedRow = update(rowToUpdate, {$merge: updated});
+      rows[i] = updatedRow;
+    }
+
+    this.setState({ rows });
+  };
+
+  handleAddRow = ({ newRowIndex }) => {
+    const newRow = {
+      value: newRowIndex,
+      userStory: '',
+      developer: '',
+      epic: ''
+    };
+
+    let rows = this.state.rows.slice();
+    rows = update(rows, {$push: [newRow]});
+    this.setState({ rows });
+  };
+
+  getRowAt = (index) => {
+    if (index < 0 || index > this.getSize()) {
+      return undefined;
+    }
+
+    return this.state.rows[index];
+  };
+
+  getSize = () => {
+    return this.state.rows.length;
+  };
+
+  getColumns = () => {
+    let clonedColumns = this._columns.slice();
+    clonedColumns[2].events = {
+      onClick: (ev, args) => {
+        const idx = args.idx;
+        const rowIdx = args.rowIdx;
+        this.grid.openCellEditor(rowIdx, idx);
+      }
+    };
+    return clonedColumns;
+  };
+
+  render() {
+    return (
+
+      <div>
+        <div style={{display: 'flex', marginBottom: '10px', alignItems: 'center'}}>
+          <span style={{marginRight: '10px'}}>Row Index: </span>
+          <input
+            style={{marginRight: '10px', border: '1px outset lightgray', padding: '3px'}}
+            type='text'
+            value={this.state.value}
+            onChange={(event) => { this.setState({value: event.target.value})}} />
+          <button
+            style={{padding: '5px'}}
+            onClick={() => this.setState({scrollToRowIndex: this.state.value == 1 ? -1 : (this.state.value-1)})}>Scroll to row</button>
+        </div>
+        <ReactDataGrid
+          ref={ node => this.grid = node }
+          onGridSort={this.handleGridSort}
+          enableCellSelect={true}
+          columns={this._columns}
+          rowGetter={this.rowGetter}
+          rowsCount={this.getSize()}
+          onGridRowsUpdated={this.handleGridRowsUpdated}
+          toolbar={<Toolbar enableFilter={true} onAddRow={this.handleAddRow}/>}
+          onAddFilter={this.handleFilterChange}
+          onClearFilters={this.onClearFilters} 
+          enableRowSelect={true}
+          scrollToRowIndex={this.state.scrollToRowIndex} 
+          rowHeight={50}
+          minHeight={600}
+          rowScrollTimeout={200} />
+      </div>
+    );
+  }
+}
+
+module.exports = exampleWrapper({
+  WrappedComponent: Example,
+  exampleName: 'All the features grid',
+  exampleDescription: 'This example demonstrates all the features from the previous examples.',
+  examplePath: './scripts/example13-all-features.js',
+  examplePlaygroundLink: undefined
+});
